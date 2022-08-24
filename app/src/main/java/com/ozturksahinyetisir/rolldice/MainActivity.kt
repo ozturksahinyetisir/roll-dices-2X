ea3769feebbd2f9e818d00002a3843adbecb88f6
Öztürk Şahin Yetişir <ozturksahinyetisir@gmail.com>
2022-08-24 10:20:45 +0300

    added some comments

--- app/src/main/java/com/ozturksahinyetisir/rolldice/MainActivity.kt
+++ app/src/main/java/com/ozturksahinyetisir/rolldice/MainActivity.kt
@@ -2,18 +2,25 @@ package com.ozturksahinyetisir.rolldice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import java.util.*
 
 
+
 class MainActivity : AppCompatActivity() {
+    // variables
     private lateinit var button: Button
     private lateinit var imageView:ImageView
     private lateinit var imageView2:ImageView
     private lateinit var TextView1:TextView
     private lateinit var TextView2:TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,11 +32,16 @@ class MainActivity : AppCompatActivity() {
 
 
         button.setOnClickListener{
+            // when button clicked calling these functions.
             rollDices()
             rollDices2()
+            Toast.makeText(this, "Zar atılıyor.", Toast.LENGTH_SHORT).show()
         }
     }
+         // function 1
     private fun rollDices(){
+             // rolls 1 to 6 random number.
+        val animation: Animation = AnimationUtils.loadAnimation(this,R.anim.rotate)
         val randomInt = Random().nextInt(6)+1
         TextView1.text = "  ${randomInt}  ve"
         val drawableResource = when(randomInt){
@@ -38,14 +50,18 @@ class MainActivity : AppCompatActivity() {
             3 -> R.drawable.ic_dice3
             4 -> R.drawable.ic_dice4
             5 -> R.drawable.ic_dice5
-            6 -> R.drawable.ic_dice6
-            else -> {R.drawable.ic_dice1}
+            else -> R.drawable.ic_dice6
+
         }
+        imageView.startAnimation(animation)
         imageView.setImageResource(drawableResource)
 
     }
-
+        // function 2
     private fun rollDices2(){
+            // rolls 1 to 6 random number for 2nd dice.
+
+        val animation: Animation = AnimationUtils.loadAnimation(this,R.anim.rotate)
         val randomInt = Random().nextInt(6)+1
         TextView2.text = "${randomInt} atıldı.  "
         val drawableResource = when(randomInt){
@@ -54,10 +70,9 @@ class MainActivity : AppCompatActivity() {
             3 -> R.drawable.ic_dice3
             4 -> R.drawable.ic_dice4
             5 -> R.drawable.ic_dice5
-            6 -> R.drawable.ic_dice6
-            else -> {R.drawable.ic_dice1}
+            else -> R.drawable.ic_dice6
         }
-
+        imageView2.startAnimation(animation)
         imageView2.setImageResource(drawableResource)
     }
 }
